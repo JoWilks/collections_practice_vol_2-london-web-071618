74b963bf8520147c69a3937b364f305252c169a9
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-04 15:44:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|element| element[0] == "r" }
+end
+
+def contain_a(array) 
+  array.select {|word| word.include? "a"}
+end
+
+def first_wa(array)
+  array.find { |word| (word[0] == "w" && word[1] == "a") }
+end
+
+def remove_non_strings(array)
+  array.select {|element| element.is_a? String }
+end
+
+def count_elements(array)
+  array.each_with_object(Hash.new(0)) { |word,count| count[word] += 1 }
+end
+
+def merge_data(keys, data)
+    
+end
+  
